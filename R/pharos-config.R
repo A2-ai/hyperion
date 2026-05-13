13f2e1829d412df3488a79803640bdd05c5ab869
Matt Smith <msmith21794@gmail.com>
2026-05-12 21:46:03 -0400

    update use_comments. touch up vignettes

--- R/pharos-config.R
+++ R/pharos-config.R
@@ -1,16 +1,20 @@
-#' Set comment type to type1 in pharos.toml
+#' Set comment parsing type in pharos.toml
 #'
-#' Modifies the pharos.toml configuration file to use type1 comment parsing.
-#' This is useful when NONMEM control streams use the type1 comment format.
+#' Writes `type = <value>` under the `[nonmem.comments]` section of
+#' `pharos.toml`.
 #'
+#' @param type Comment parsing type. One of `"type1"` (strict structured) or
+#'   `"type2"` (flexible structured grammar).
 #' @param path Path to pharos.toml. If NULL, finds it automatically.
 #' @return The path to the modified pharos.toml file (invisibly).
 #' @export
 #'
 #' @examples \dontrun{
-#' use_type1_comments()
+#' use_comments("type2")
 #' }
-use_type1_comments <- function(path = NULL) {
+use_comments <- function(type = c("type1", "type2"), path = NULL) {
+  type <- match.arg(type)
+
   if (is.null(path)) {
     path <- find_pharos_config_file()
     if (grepl("No pharos.toml", path)) {
@@ -20,9 +24,16 @@ use_type1_comments <- function(path = NULL) {
 
   toml <- tomledit::read_toml(path)
   nonmem <- tomledit::get_item(toml, "nonmem")
-  nonmem$comments$type <- "type1"
+  nonmem$comments$type <- type
   toml <- tomledit::insert_items(toml, nonmem = nonmem)
   tomledit::write_toml(toml, path)
 
   invisible(path)
+}
+
+#' @rdname use_comments
+#' @export
+use_type1_comments <- function(path = NULL) {
+  .Deprecated("use_comments", old = "use_type1_comments")
+  use_comments(type = "type1", path = path)
 }
